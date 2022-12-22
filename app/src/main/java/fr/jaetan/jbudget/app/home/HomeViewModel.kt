6c5a75a526cfa664b281b77cdd1607cd2c7933e1
Nako <Nanaka.kumiho@gmail.com>
2022-12-22 08:43:20 +0100

    FAB UI Completed

--- app/src/main/java/fr/jaetan/jbudget/app/home/HomeViewModel.kt
+++ app/src/main/java/fr/jaetan/jbudget/app/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package fr.jaetan.jbudget.app.home
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NoteAdd
+import androidx.compose.material.icons.filled.RequestQuote
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,7 +12,6 @@ import fr.jaetan.jbudget.R
 class HomeViewModel : ViewModel() {
     var fabExpanded by mutableStateOf(false)
     val fabItems = listOf(
-        FabItem(text = R.string.home_fab_add_budget, descriptor = R.string.home_fab_add_budget_descriptor, onClick = {}, Icons.Default.NoteAdd),
-        FabItem(text = R.string.home_fab_add_transaction, descriptor = R.string.home_fab_add_transaction_descriptor, onClick = {}, Icons.Default.NoteAdd),
-    )
+        FabItem(text = R.string.home_fab_add_transaction, descriptor = R.string.home_fab_add_transaction_descriptor, onClick = {}, Icons.Default.RequestQuote),
+        FabItem(text = R.string.home_fab_add_budget, descriptor = R.string.home_fab_add_budget_descriptor, onClick = {}, Icons.Default.NoteAdd),)
 }
--- app/src/main/java/fr/jaetan/jbudget/app/home/views/HomeFAB.kt
+++ app/src/main/java/fr/jaetan/jbudget/app/home/views/HomeFAB.kt
@@ -1,19 +1,21 @@
 package fr.jaetan.jbudget.app.home.views
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import fr.jaetan.jbudget.R
 import fr.jaetan.jbudget.app.home.HomeViewModel
 
 @Composable
 fun HomeFAB(viewModel: HomeViewModel) {
-    Column() {
-        Column {
+    Column(verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.End) {
+        Column(horizontalAlignment = Alignment.End) {
             HomeFabContainer(viewModel)
         }
         FloatingActionButton(
@@ -24,15 +26,21 @@ fun HomeFAB(viewModel: HomeViewModel) {
             Icon(Icons.Filled.Add, contentDescription = stringResource(R.string.home_fab_add_descriptor))
         }
     }
+
 }
 
 
 @Composable
 fun HomeFabContainer(viewModel: HomeViewModel) {
-}
-
-
-@Composable
-fun HomeFABItem() {
-
+    if(viewModel.fabExpanded)
+    {
+        for(item in viewModel.fabItems) {
+            Row(horizontalArrangement = Arrangement.End, verticalAlignment = Alignment.CenterVertically) {
+                Text(stringResource(item.text))
+                FilledIconButton(onClick = item.onClick) {
+                    Icon(imageVector = item.icon, contentDescription = stringResource(item.descriptor))
+                }
+            }
+        }
+    }
 }
