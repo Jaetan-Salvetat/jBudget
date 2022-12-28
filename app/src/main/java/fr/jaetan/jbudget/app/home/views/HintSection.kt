764939e15f87e0447ec73836e6c9beaba05c5507
gaetan16 <gaetan@stock-pro.fr>
2022-12-28 01:00:36 +0100

    Revert "Creating Complete Hint System"
    This reverts commit cb821d410acc27b5dce79dba4beab99c404e94fa.

--- app/src/main/java/fr/jaetan/jbudget/app/home/views/HintSection.kt
+++ app/src/main/java/fr/jaetan/jbudget/app/home/views/HintSection.kt
@@ -1,9 +1,10 @@
 package fr.jaetan.jbudget.app.home.views
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import fr.jaetan.jbudget.app.home.HomeViewModel
 
 @Composable
-fun HintSection(viewModel: HomeViewModel) {
-    viewModel.hintItems[viewModel.currentHint].GetView(viewModel)
+fun HintSection() {
+    Column {
+    }
 }
--- app/src/main/java/fr/jaetan/jbudget/app/home/views/HomeContent.kt
+++ app/src/main/java/fr/jaetan/jbudget/app/home/views/HomeContent.kt
@@ -4,17 +4,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import fr.jaetan.jbudget.app.home.HomeViewModel
 
 @Composable
-fun HomeContent(padding: PaddingValues, viewModel: HomeViewModel) {
+fun HomeContent(padding: PaddingValues) {
     Column(
         modifier = Modifier
             .padding(padding)
             .padding(horizontal = 20.dp)
             .fillMaxSize(),
     ) {
-        HintSection(viewModel)
-        Spacer(modifier = Modifier.height(20.dp))
+        HintSection()
     }
 }
