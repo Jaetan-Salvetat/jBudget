44ad95b20c62072786abf1287bb0d1716d21419b
gaetan16 <gaetan@stock-pro.fr>
2023-01-13 16:58:11 +0100

    Add a bottom padding to the LazyColumn

--- app/src/main/java/fr/jaetan/jbudget/app/home/views/HomeContent.kt
+++ app/src/main/java/fr/jaetan/jbudget/app/home/views/HomeContent.kt
@@ -1,9 +1,13 @@
 package fr.jaetan.jbudget.app.home.views
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import fr.jaetan.jbudget.app.home.HomeViewModel
 
 @Composable
@@ -14,5 +18,6 @@ fun HomeContent(viewModel: HomeViewModel) {
         items(viewModel.getCurrentBudgets()) { HomeBudgetsListItem(it, viewModel) }
         item { Divider() }
         items(viewModel.getOldBudgets()) { HomeBudgetsListItem(it, viewModel) }
+        item { Spacer(Modifier.height(100.dp)) }
     }
 }
