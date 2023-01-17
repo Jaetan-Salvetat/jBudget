f6ad81d2db495a8a8f81ca17a1bffd5d705623a3
Nako <Nanaka.kumiho@gmail.com>
2023-01-17 11:41:22 +0100

    Adding Id in Transaction.kt, createTransaction in TransactionRepository.kt, CategoryRepository.kt with its getAll function, Editing dates in BudgetContent.kt

--- app/src/main/java/fr/jaetan/jbudget/core/repositories/CategoryRepository.kt
+++ app/src/main/java/fr/jaetan/jbudget/core/repositories/CategoryRepository.kt
@@ -5,8 +5,8 @@ import com.google.firebase.ktx.Firebase
 import fr.jaetan.jbudget.core.models.FirebaseResponse
 import fr.jaetan.jbudget.core.models.Transaction
 
-class TransactionRepository {
-    private val database = Firebase.firestore.collection("transactions")
+class CategoryRepository {
+    private val database = Firebase.firestore.collection("categories")
 
     fun getAll(budgetId: String?, callback: (List<Transaction>, FirebaseResponse) -> Unit) {
         database
@@ -17,19 +17,6 @@ class TransactionRepository {
                     return@addOnCompleteListener
                 }
                 callback(listOf(), FirebaseResponse.Error)
-            }
-    }
-
-    fun createTransaction(transaction: Transaction, callback: (String?, FirebaseResponse) -> Unit) {
-        database
-            .add(transaction.toMap())
-            .addOnSuccessListener { documentReference ->
-                callback(documentReference.id, FirebaseResponse.Success) }
-            .addOnCanceledListener {
-                callback(null, FirebaseResponse.ConnectivityError)
-            }
-            .addOnFailureListener {
-                callback(null, FirebaseResponse.Error)
             }
     }
 }
